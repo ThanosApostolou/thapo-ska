b5d5c5b240e348d12c67e601c2ff9a359fdd6841
ThanosApostolou <thanosapostolou@outlook.com>
2024-03-18 11:24:09 +0200

    attempt to fix kubernetes

--- devops/scripts/ctr-import.sh
+++ devops/scripts/ctr-import.sh
@@ -3,32 +3,22 @@ set -eu
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 cd "$SCRIPT_DIR"
 
-if [ ! $# -eq 1 ]
-  then
-    echo "Error: Usage: bash helm-build.sh profile(local/dev/prod)"
-    exit 1
-fi
-
-profile="$1"
-if ! [ "$profile" = "local" ] && ! [ "$profile" = "dev" ] && ! [ "$profile" = "prod" ]; then
-    echo "profile should be local/dev/prod"
-    exit 1
-fi
 
 function ctr_import() {
     image="$1"
+    echo "image: $image"
     sudo docker save "$image" | sudo ctr -a /var/run/k3s/containerd/containerd.sock -n k8s.io images import -
 }
 
 
 cd "$SCRIPT_DIR/../../ska_backend"
 backend_version="$(echo "$(cargo pkgid)" | rev | cut -d'#' -f1 | rev)"
-ctr_import "registry.thapo-dev.org:5000/thapo/thapo_ska_backend:$backend_version-$profile"
+ctr_import "registry.thapo-dev.org:5000/thapo/thapo_ska_backend:$backend_version-local_kube"
 
 
 cd "$SCRIPT_DIR/../../ska_frontend"
 frontend_version="$(echo "$(cargo pkgid)" | rev | cut -d'#' -f1 | rev)"
-ctr_import "registry.thapo-dev.org:5000/thapo/thapo_ska_frontend:$frontend_version-$profile"
+ctr_import "registry.thapo-dev.org:5000/thapo/thapo_ska_frontend:$frontend_version-local_kube"
 
-ctr_import "registry.thapo-dev.org:5000/thapo/thapo_ska_gateway:$frontend_version-$profile"
-ctr_import "registry.thapo-dev.org:5000/thapo/thapo_ska_iam:$frontend_version-$profile"
+ctr_import "registry.thapo-dev.org:5000/thapo/thapo_ska_gateway:$frontend_version-local_kube"
+ctr_import "registry.thapo-dev.org:5000/thapo/thapo_ska_iam:$frontend_version-local_kube"
