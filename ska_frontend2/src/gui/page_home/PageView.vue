81ecd1dc3dd9a1138fb778302b6362b6d608f76a
ThanosApostolou <thanosapostolou@outlook.com>
2024-05-02 16:50:24 +0300

    initial ska_frontend2 in vue

---
+++ ska_frontend2/src/gui/page_home/PageView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    page home
+  </main>
+</template>
---
+++ ska_frontend2/src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../gui/page_home/PageView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../gui/page_about/PageAbout.vue')
+    }
+  ]
+})
+
+export default router
